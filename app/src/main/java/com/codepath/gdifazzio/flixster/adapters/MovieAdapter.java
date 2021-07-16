e8da942d8607d285b38e3340532f53c7777da3fb
gdifazzio98 <gdifazzio98@gmail.com>
2021-07-16 18:49:26 -0500

    initial commit

--- app/src/main/java/com/codepath/gdifazzio/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/gdifazzio/flixster/adapters/MovieAdapter.java
@@ -13,7 +13,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
